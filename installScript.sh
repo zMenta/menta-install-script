bf1733c53b27ebd20eeb95751b7aa8b0636237e4
Lucas Brentano <brentano.l@gmail.com>
2022-12-25 00:29:04 -0300

    added brave browser to downloads

--- installScript.sh
+++ installScript.sh
@@ -20,6 +20,15 @@ done
 
 # sudo apt install vim -y
 # sudo apt install htop -y
+# sudo apt install emacs -y
+
+# Brave Browser
+# sudo apt install curl
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# sudo apt update
+# sudo apt install brave-browser
+
 
 if [ $install_latex ]; then
     #sudo apt install texlive-full -y
