5ec6816689f4e82e39e11bbe21dfacc6c649e627
Lucas Brentano <brentano.l@gmail.com>
2022-12-24 23:52:36 -0300

    bash if statements

--- installScript.sh
+++ installScript.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
+declare install_latex="yes"
+install_latex="$1"
+
+if [[ $install_latex == "no-latex"  ]]; then
+    echo "Latex is not going to be installed"
+elif [[ $install_latex == "" ]]; then
+     echo "Running default script order"
+fi
+
+# sudo apt update -y
+# sudo apt upgrade -y
+
+# sudo apt install vim -y
+# sudo apt install htop -y
+
+# echo "$install_latex"
+
+echo "Script finished running. All done."
