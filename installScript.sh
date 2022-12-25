a349fe400f7c87b9ed870e877b09a0dba19d80e4
Lucas Brentano <brentano.l@gmail.com>
2022-12-25 00:40:20 -0300

    added doom emacs and git

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare install_latex=1 #true
+declare install_doom_emacs=1
 
 # Cycle through the variables passed
 while [ "$#" -gt 0 ]; do
@@ -8,8 +9,12 @@ while [ "$#" -gt 0 ]; do
 
     # Latex Download
     if [[ $argument == "no-latex"  ]]; then
-        echo "Latex is not going to be installed"
-        install_latex=1
+        install_latex=0
+    fi
+
+    # Doom Emacs Download
+    if [[ $argument == "no-doom"  ]]; then
+        install_doom_emacs=0
     fi
 
     shift
@@ -18,6 +23,7 @@ done
 # sudo apt update -y
 # sudo apt upgrade -y
 
+# sudo apt install git -y
 # sudo apt install ripgrep -y
 # sudo apt install vim -y
 # sudo apt install htop -y
@@ -34,6 +40,13 @@ done
 if [ $install_latex ]; then
     #sudo apt install texlive-full -y
     echo "texlive-full going to be installed"
+fi
+
+if [ $install_doom_emacs ]; then
+    echo "Doom installation begins"
+    # git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+    # ~/.emacs.d/bin/doom install
+    echo "doom emacs going to be installed"
 fi
 
 echo "Script finished running. All done."
