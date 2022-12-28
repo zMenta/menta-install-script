743456b82109adc467b72aad316f4ed47d1d1fd3
Lucas Brentano <brentano.l@gmail.com>
2022-12-28 11:52:35 -0300

    script bugfix and readme update

--- installScript.sh
+++ installScript.sh
@@ -3,23 +3,26 @@
 declare install_latex=1 #true
 declare install_doom_emacs=1
 declare install_reveal_js=1
+declare install_doom_default=0
 
 # Cycle through the variables passed
 while [ "$#" -gt 0 ]; do
     argument="$1"
 
-    # Latex Download
     if [[ $argument == "no-latex"  ]]; then
         install_latex=0
     fi
 
-    # Doom Emacs Download
     if [[ $argument == "no-doom"  ]]; then
         install_doom_emacs=0
     fi
 
     if [[ $argument == "no-revealjs"  ]]; then
         install_reveal_js=0
+    fi
+
+    if [[ $argument == "doom-default"  ]]; then
+        install_doom_default=1
     fi
 
     shift
@@ -42,12 +45,12 @@ done
 # sudo apt install brave-browser -y
 
 
-if [ $install_latex ]; then
+if [ $install_latex -eq 1 ]; then
     #sudo apt install texlive-full -y
     echo "latex"
 fi
 
-if [ $install_reveal_js ]; then
+if [ $install_reveal_js -eq 1 ]; then
     # Reveal JS and Dependencies
     # -NodeJs
     # curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
@@ -60,10 +63,14 @@ if [ $install_reveal_js ]; then
     echo "reveal js"
 fi
 
-if [ $install_doom_emacs ]; then
+if [ $install_doom_emacs -eq 1 ]; then
     # git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
     # ~/.emacs.d/bin/doom install
     echo "doom emacs"
+fi
+
+if [ $install_doom_default -eq 0 ]; then
+    echo "Installed menta doom config"
 fi
 
 # sudo apt update -y
